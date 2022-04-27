496b9009869315ed58770fd181c7ff7d9b63113d
Callum Leslie <github@cleslie.uk>
2022-04-27 18:51:15 +0100

    Environment packages

--- install.sh
+++ install.sh
@@ -3,3 +3,13 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
 # source nix
 . ~/.nix-profile/etc/profile.d/nix.sh
+
+# install environment packages
+nix-env -iA \
+        nixpkgs.git \
+        nixpkgs.zsh \
+        nixpkgs.stow \
+        nixpkgs.bat \
+        nixpkgs.direnv \
+        nixpkgs.antibody \
+        nixpkgs.fzf 
